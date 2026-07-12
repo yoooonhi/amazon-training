90efa10ba0e10cdfbb997b22fc1d5daef41324cf
yoooonhi <13049277922@163.com>
2026-07-12 13:37:14 +0800

    修复 favicon 路径适配 GitHub Pages 子路径

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   base: '/amazon-training/',
 
   head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/amazon-training/logo.svg' }],
   ],
 
   themeConfig: {
